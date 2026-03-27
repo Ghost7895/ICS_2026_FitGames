714af85572772d540aaa77caa2fb4c4fff6720ea
Safy555 <jakubkralik555@gmail.com>
2026-03-27 17:53:47 +0100

    added Mappers + Repository layer for DAL #ISSUE-8

---
+++ FitGames/FitGames.DAL/Mappers/IEntityMapper.cs
@@ -0,0 +1,10 @@
+﻿using FitGames.DAL.Entities;
+
+namespace FitGames.DAL.Mappers
+{
+    public interface IEntityMapper<in TEntity>
+        where TEntity : IEntity
+    {
+        void MapToExistingEntity(TEntity existingEntity, TEntity newEntity);
+    }
+}
