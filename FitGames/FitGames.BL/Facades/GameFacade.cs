d2202bcc0d5b04c9f16cfb7aa690d2ffc73f25aa
Krucek05 <krucek2005@gmail.com>
2026-05-08 21:57:39 +0200

    Added filters by genre,pegi, name, imporved UI

--- FitGames/FitGames.BL/Facades/GameFacade.cs
+++ FitGames/FitGames.BL/Facades/GameFacade.cs
@@ -11,7 +11,7 @@ namespace FitGames.BL.Facades;
 public class GameFacade(
     IUnitOfWorkFactory unitOfWorkFactory,
     GameModelMapper modelMapper)
-    : FacadeBase<GameEntity, GameListModel, GameDetailModel, GameEntityMapper>(unitOfWorkFactory, modelMapper), 
+    : FacadeBase<GameEntity, GameListModel, GameDetailModel, GameEntityMapper>(unitOfWorkFactory, modelMapper),
         IGameFacade
 {
     protected override ICollection<string> IncludesNavigationPathDetail =>
@@ -33,5 +33,25 @@ public class GameFacade(
                                 .GetAllAsync(filter: g => g.Genre == genre);
 
         return ModelMapper.MapToListModel(entities);
+    }
+
+    public async Task<IEnumerable<GameListModel>> FilterGamesByPegiAsync(Pegi pegi)
+    {
+        await using IUnitOfWork uow = UnitOfWorkFactory.Create();
+        var entities = await uow.GetRepository<GameEntity, GameEntityMapper>()
+                                .GetAllAsync(filter: g => g.Pegi == pegi);
+
+        return ModelMapper.MapToListModel(entities);
+    }
+
+    public async Task<IEnumerable<GameListModel>> GetGamesSortedByNameAsync(bool ascending = true)
+    {
+        await using IUnitOfWork uow = UnitOfWorkFactory.Create();
+        var entities = await uow.GetRepository<GameEntity, GameEntityMapper>()
+                                .GetAllAsync();
+
+        return ascending
+            ? ModelMapper.MapToListModel(entities.OrderBy(g => g.Name))
+            : ModelMapper.MapToListModel(entities.OrderByDescending(g => g.Name));
     }
 }
---
+++ FitGames/FitGames.app/Converters/BoolToSortLabelConverter.cs
@@ -0,0 +1,12 @@
+﻿using System.Globalization;
+
+namespace FitGames.app.Converters;
+
+public class BoolToSortLabelConverter : IValueConverter
+{
+    public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        => value is true ? "A→Z" : "Z→A";
+
+    public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        => throw new NotImplementedException();
+}
